567052c0ac48ee8293af8201cfdd1a1ae9267a40
vvliebe <vvliebe@gmail.com>
2018-07-02 03:11:33 +0800

    feat: add t10

--- src/T10.js
+++ src/T10.js
@@ -5,5 +5,18 @@
  * @return {boolean}
  */
 var isMatch = function(s, p) {
-    
+  if (p.length === 0) return s.length === 0
+  if (p.length === 1) return s.length === 1 && (s[0] === p[0] || p[0] === '.')
+  if (p[1] !== '*') {
+    if (s.length === 0) return false
+    return (s[0] === p[0] || p[0] === '.') && isMatch(s.substring(1), p.substring(1))
+  } else {
+    while (s.length > 0 && (s[0] === p[0] || p[0] === '.')) {
+      if (isMatch(s, p.substring(2))) return true
+      s = s.substring(1)
+    }
+    return isMatch(s, p.substring(2))
+  }
 };
+
+console.log(isMatch('abbssccc',  'ab*s*c*'))
